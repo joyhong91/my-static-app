83a39f5ecdf11c59f3564626b56912b087c26fc3
glory.h <glory.h@kakaocorp.com>
2024-09-23 20:36:04 +0900

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+export default defineNuxtConfig({
+  // Global page headers
+  app: {
+    head: {
+      title: 'My Nuxt 3 Project',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+    },
+  },
+
+  compatibilityDate: '2024-09-23'
+})
